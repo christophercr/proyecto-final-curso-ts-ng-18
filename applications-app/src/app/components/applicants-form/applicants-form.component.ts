02fa0dcdde50c8b7d1ce8f158bd13ecb372741eb
Jorge Álvarez <72515345+Jrgmadcontactosprofesionales@users.noreply.github.com>
2024-09-30 17:39:36 +0200

    2 - Componente applicants-form-component utilizado en app-component

---
+++ applications-app/src/app/components/applicants-form/applicants-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-applicants-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './applicants-form.component.html',
+  styleUrl: './applicants-form.component.css'
+})
+export class ApplicantsFormComponent {
+
+}
