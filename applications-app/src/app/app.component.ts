4c25a7f55cee9d2c80159229da7fc05f766502b8
Jorge Álvarez <72515345+Jrgmadcontactosprofesionales@users.noreply.github.com>
2024-09-30 17:39:36 +0200

    2 - Componente applicants-form-component utilizado en app-component

--- applications-app/src/app/app.component.ts
+++ applications-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApplicantsFormComponent } from './components/applicants-form/applicants-form.component'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ApplicantsFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ applications-app/src/app/components/applicants-form/applicants-form.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ApplicantsFormComponent } from './applicants-form.component';
+
+describe('ApplicantsFormComponent', () => {
+  let component: ApplicantsFormComponent;
+  let fixture: ComponentFixture<ApplicantsFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ApplicantsFormComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ApplicantsFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
