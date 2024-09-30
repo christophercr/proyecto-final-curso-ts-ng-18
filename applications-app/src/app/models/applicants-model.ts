2b5ca5cf59dd8827b581e7c31f711f0a2c7b8fdb
Jorge Álvarez <72515345+Jrgmadcontactosprofesionales@users.noreply.github.com>
2024-09-30 20:06:53 +0200

    Se crea un modelo y se instancia desde el servicio

---
+++ applications-app/src/app/models/applicants-model.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Status } from '../constants/application-status.constant';
+
+export class ApplicantsModel {
+  name!: string;
+  email!: string;
+  age!: number;
+  yearsExp!: number;
+  position!: string;
+  dateOfApplication!: string;
+  status!: Status;
+
+
+/*   constructor(
+    name: string,
+    email: string,
+    age: number,
+    yearsExp: number,
+    position: string,
+    dateOfApplication: string,
+    status: Status
+  ) {
+    this.name = name;
+    this.email = email;
+    this.age = age;
+    this.yearsExp = this.yearsExp;
+    this.position = this.position;
+    this.status = this.status;
+  } */
+}
