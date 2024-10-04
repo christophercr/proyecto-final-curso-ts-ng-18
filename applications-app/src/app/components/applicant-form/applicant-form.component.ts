add11a6f5daf7d2d2365a6a387476346a0810442
Jorge Álvarez <72515345+Jrgmadcontactosprofesionales@users.noreply.github.com>
2024-10-04 22:37:45 +0200

    El formulario ya recoge datos aunque no tiene formato

--- applications-app/src/app/components/applicant-form/applicant-form.component.ts
+++ applications-app/src/app/components/applicant-form/applicant-form.component.ts
@@ -4,7 +4,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { Status } from '../../constants/application-status.constant';
 import { ApplicantModel } from '../../models/applicant.model';
-import { FormGroup, ReactiveFormsModule} from '@angular/forms';
+import { FormGroup, NonNullableFormBuilder, ReactiveFormsModule} from '@angular/forms';
 import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
@@ -14,8 +14,11 @@ import { FormBuilder, Validators } from '@angular/forms';
   templateUrl: './applicant-form.component.html',
   styleUrl: './applicant-form.component.css',
 })
+
 export class ApplicantsFormComponent {
+
   fb = inject(FormBuilder);
+
   applicantForm = this.fb.group({
     name: ['', Validators.required],
     email: ['', [Validators.required, Validators.email]],
@@ -25,10 +28,9 @@ export class ApplicantsFormComponent {
     position: ['', Validators.required],
     dateOfApplication: ['', Validators.required],
     status: ['', Validators.required],
-  }); ;
-  statuses = Object.values(Status);
+  }); 
 
-  /* applicant : ApplicantModel; */
+  statuses = Object.values(Status);
 
   onSubmit() {
     if (this.applicantForm.valid) {
