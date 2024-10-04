39d3d491a5827ba29dd888d28f2b1258c07d331e
srueda1 <srueda@agroseguro.es>
2024-10-04 13:29:39 +0200

    04/10/24 segunda avance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SolicitudListadoComponent } from "./components/solicitud-listado/solicitud-listado.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SolicitudListadoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
