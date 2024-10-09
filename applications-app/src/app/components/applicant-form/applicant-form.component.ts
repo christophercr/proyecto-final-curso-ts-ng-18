55fb8674a53dc1c55adf0e03945cc54d29cceadd
Jorge Álvarez <72515345+Jrgmadcontactosprofesionales@users.noreply.github.com>
2024-10-09 23:03:18 +0200

    Se comienza la implementación del servicio. Compila pero no muestra lo que recoge del formulario al haber cambiado cosas en el ApplicationsForm.ts para adaptarlo al service

--- applications-app/src/app/components/applicant-form/applicant-form.component.ts
+++ applications-app/src/app/components/applicant-form/applicant-form.component.ts
@@ -1,32 +1,94 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Output, EventEmitter } from '@angular/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatCommonModule, provideNativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import {
+  MatCommonModule,
+  provideNativeDateAdapter,
+} from '@angular/material/core';
 import { MatMomentDateModule } from '@angular/material-moment-adapter';
 import { Status } from '../../constants/application-status.constant';
 import { ApplicantModel } from '../../models/applicant.model';
-import { ReactiveFormsModule, FormsModule, AsyncValidatorFn, AbstractControl, ValidationErrors} from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { FormBuilder, Validators } from '@angular/forms';
-import { Moment } from 'moment';
-import { CommonModule } from '@angular/common';
-import { Observable } from 'rxjs';
+import { ApplicationsService } from '../../services/applications.service';
 
 @Component({
   selector: 'app-applicants-form',
   standalone: true,
   providers: [provideNativeDateAdapter()],
-  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatSelectModule, MatDatepickerModule, MatMomentDateModule, ReactiveFormsModule, FormsModule, MatCommonModule],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatMomentDateModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatCommonModule,
+  ],
   templateUrl: './applicant-form.component.html',
   styleUrl: './applicant-form.component.css',
 })
-
-
 export class ApplicantsFormComponent {
+  private readonly _applicantservice = inject(ApplicationsService);
 
- /*  applicantMod = new ApplicantModel(
+  @Output()
+  created: EventEmitter<ApplicantModel> = new EventEmitter<ApplicantModel>();
+
+  fb = inject(FormBuilder);
+
+  applicantForm = this.fb.group({
+    name: ['', [Validators.required, Validators.min(2), Validators.max(120)]],
+    email: ['', [Validators.required, Validators.email]],
+    age: ['', [Validators.required, Validators.min(18), Validators.max(120)]],
+    yearsExp: [
+      '',
+      [Validators.required, Validators.min(0), Validators.max(100)],
+    ],
+    position: ['', Validators.required],
+    dateOfApplication: [null, Validators.required],
+    status: ['', Validators.required],
+  });
+
+  statuses = Object.values(Status);
+
+  createApplicant(): void {
+    if (this.applicantForm.valid) {
+      /*  const applicant: ApplicantModel = this.applicantForm.value;
+      console.log('applicant sent:', applicant);  */
+
+      const rawValue: any = this.applicantForm.getRawValue();
+
+      for (const key of Object.keys(rawValue)) {
+        rawValue[key] = rawValue[key] === null ? undefined : rawValue[key];
+      }
+
+      const applicantToCreate: ApplicantModel = new ApplicantModel(
+        rawValue.name,
+        rawValue.email,
+        rawValue.age,
+        rawValue.yearsExp,
+        rawValue.position,
+        rawValue.dateOfApplication,
+        rawValue.status
+      );
+    }
+    /* 
+      this._applicantservice
+        .createApplicant(applicantToCreate)
+        .then(() => {
+          this.created.emit(applicantToCreate);
+          this.applicantForm.reset();
+        });
+    } */
+  }
+} // Fin de clase
+
+/*  applicantMod = new ApplicantModel(
     'Manolo',
     'manolete@manolete.com',
     33,
@@ -35,33 +97,3 @@ export class ApplicantsFormComponent {
     '06/10/2024',
     Status.Approved,
   ); */
-
-
-  
-
-  fb = inject(FormBuilder);
-
-  applicantForm = this.fb.group({
-    name: ['', [Validators.required, Validators.min(2), Validators.max(120)]],
-    email: ['', [Validators.required, Validators.email]],
-    age: ['', [Validators.required, Validators.min(18), Validators.max(120)]],
-    yearsExp: [  '', [Validators.required, Validators.min(0), Validators.max(100)],
-    ],
-    position: ['', Validators.required],
-    dateOfApplication: [null, Validators.required],
-    status: ['', Validators.required],
-  }); 
-
-  statuses = Object.values(Status);
-
-  onSubmit() {
-    if (this.applicantForm.valid) {
-      const usuario: ApplicantModel = this.applicantForm.value;
-      console.log('Usuario enviado:', usuario);
-      // ¿Gestionar la lógica de envío servidor?
-    }
-  }
-
-
-
-}
