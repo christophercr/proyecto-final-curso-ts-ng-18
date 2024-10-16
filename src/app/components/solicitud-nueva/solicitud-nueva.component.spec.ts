6140e8573a2bf9e700a79389eea3b184a61520a5
srueda1 <srueda@agroseguro.es>
2024-10-16 21:03:57 +0200

    16/10/24: test pagina-inicio.component.spec

--- src/app/components/solicitud-nueva/solicitud-nueva.component.spec.ts
+++ src/app/components/solicitud-nueva/solicitud-nueva.component.spec.ts
@@ -1,12 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SolicitudNuevaComponent } from './solicitud-nueva.component';
+import { SolicitudService } from '../../services/solicitud.service';
+import { Solicitud } from '../../models/solicitud.model';
+import { signal } from '@angular/core';
 
-describe('SolicitudNuevaComponent', () => {
+xdescribe('SolicitudNuevaComponent', () => {
   let component: SolicitudNuevaComponent;
   let fixture: ComponentFixture<SolicitudNuevaComponent>;
+  let solicitudService: jasmine.SpyObj<SolicitudService>;
+  //const solicitudCollectionsSignalStub = signal(<Map<string, <Solicitud>>(new Map());
 
   beforeEach(async () => {
+    const dummySolicitudes:Solicitud[] = [
+      new Solicitud('Sergio','sergio@gmail.com', 34, 14, 'Programador', new Date(2022, 7, 18, 0, 0, 0), 'En Espera')
+    ]; 
+    //bookCollectionsSignalStub.set(dummySolicitudes);
+    const bookServiceMock = jasmine.createSpyObj('SolicitudService', ['createBook'], {
+    //  bookCollectionsSignal: bookCollectionsSignalStub,
+    });
     await TestBed.configureTestingModule({
       imports: [SolicitudNuevaComponent]
     })
--- src/app/pages/pagina-inicio/pagina-inicio.component.spec.ts
+++ src/app/pages/pagina-inicio/pagina-inicio.component.spec.ts
@@ -1,22 +1,65 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PaginaInicioComponent } from './pagina-inicio.component';
+import { RouterTestingHarness } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import { SolicitudService } from '../../services/solicitud.service';
+import { appConfig } from '../../app.config';
+import { of } from 'rxjs';
+
 
 xdescribe('PaginaInicioComponent', () => {
   let component: PaginaInicioComponent;
   let fixture: ComponentFixture<PaginaInicioComponent>;
+  let harness: RouterTestingHarness;
+  let router: Router;
+  
+  let solicitudService: jasmine.SpyObj<SolicitudService>;
 
   beforeEach(async () => {
+    const SolicitudServiceMock = jasmine.createSpyObj('SolicitudService', ['recargarSolicitudes'], {
+      
+    });
     await TestBed.configureTestingModule({
-      imports: [PaginaInicioComponent]
+      ...appConfig,
+      imports: [PaginaInicioComponent],
+            
+      providers: [
+        ...appConfig.providers, // cogemos los providers que ya teníamos para no perderlos! (router + definición de rutas)
+        {
+          provide: SolicitudService,
+          useValue: SolicitudServiceMock,
+        },
+      ],
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(PaginaInicioComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    router = TestBed.inject(Router);
+    solicitudService = TestBed.inject(SolicitudService) as jasmine.SpyObj<SolicitudService>;
+    solicitudService.recargarSolicitudes.and.returnValue();    
+    harness = await RouterTestingHarness.create();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('enrutado', () => {
+    it('debe crearse cuando el router navegue a la ruta principal', async () => {
+      component = await harness.navigateByUrl('/', PaginaInicioComponent);
+
+      expect(component).toBeTruthy();
+      expect(component instanceof PaginaInicioComponent).toBeTrue();
+      expect(router.url).toBe('/');
+
+      const html = harness.routeNativeElement as HTMLElement;
+      const homePageTitle = html.querySelector<HTMLHeadingElement>('h1');
+      expect(homePageTitle?.textContent).toBe('Control de solicitudes');
+    });
+
+    it('debe destruirse cuando el router navegue a otra ruta distinta', async () => {
+      let finalComponent: PaginaInicioComponent  = await harness.navigateByUrl('/', PaginaInicioComponent);
+      expect(finalComponent instanceof PaginaInicioComponent).toBeTrue();
+
+      finalComponent = await harness.navigateByUrl('solicitudes', PaginaInicioComponent);
+
+      expect(finalComponent instanceof PaginaInicioComponent).toBeFalse();      
+      expect(router.url).toBe('/solicitud-listado'); 
+    });
   });
 });
