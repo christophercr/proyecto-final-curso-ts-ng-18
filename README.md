f3faf37d76c7b82c39930b17f5961c80226eafb0
davidof1977 <davidof1977@hotmail.com>
2024-10-19 09:14:52 +0200

    Mocking api calls

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Duda
 Una pregunta sobre los test e2e, ¿Como puedes hacer test e2e sin alterar la BDD? ¿Hay alguna manera de mockear el service que accede a la BDD?
 
+Vale, ya me he respondido ;-), he visto que puedes mockear las api call con un fixture. Gracias de todas formas. 
---
+++ solicitdudes/cypress/e2e/1-getting-started/mockingApiCalls.cy.js
@@ -0,0 +1,58 @@
+/// <reference types="cypress" />
+
+
+describe('example to-do app', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:4200')
+    cy.intercept("GET", "http://localhost:3000/solicitudes", {
+      fixture: "solicitudes-mock.json",
+    });
+  })
+
+  it('displays two todo items by default', () => {
+    cy.get('#h1Home').should('have.length', 1)
+
+    cy.get('#h1Home').first().should('have.text', 'APLICACION DE SOLICITUDES')
+  })
+
+  it('Check solicitudes', () => {
+    cy.visit('http://localhost:4200/solicitudes')
+
+    cy.get('#itemNombre').should('have.text','David Bartolome');
+    cy.get('#nombre').type('Da',{ delay: 600 });
+    cy.get('#tabla').find('tr').should('have.length',3)
+    cy.get('#nombre').type('v',{ delay: 600 });
+    cy.get('#tabla').find('tr').should('have.length',2)
+
+    cy.get('#nombre').type('XXX');
+    cy.get('#h2NoElementos').should('have.length',1)
+  })
+
+  it('Navigate to selected solicitud', () => {
+    cy.visit('http://localhost:4200/solicitudes')
+    cy.get('#tabla').find('tr').last().click()
+    cy.get('#nombre').should('contain.value','Daniel Castiilo')
+
+    cy.get('#puesto').type('test');
+
+
+  })
+
+  it('Update solicitud', () => {
+    cy.visit('http://localhost:4200/editar/5c4e')
+    cy.get('#nombre').should('contain.value','David Bartolome')
+
+    cy.get('#puesto').clear().type('test');
+    cy.get('#estado').select('rechazado')
+    cy.get('#submit').click();
+    cy.get('#tabla').find('tr').first().get('#estado').should('have.text','rechazado');
+  })
+
+  it('.new solicitante', () => {
+    cy.visit('http://localhost:4200/nueva')
+    cy.get('#nombre').focus().blur();
+    cy.get('*[class^="colorError"]').should('have.text','El campo nombre es requerido.')
+    cy.get('#submit').should('be.disabled',true)
+  })
+
+})
